34bebe1b3a87c72c2062b8c712d264ec9c6c6ed2
ietoto <ietoto@163.com>
2023-03-19 15:19:14 +0800

    operation description

---
+++ after-end/src/main/java/com/example/afterend/AfterEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.afterend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterEndApplication.class, args);
+    }
+
+}
