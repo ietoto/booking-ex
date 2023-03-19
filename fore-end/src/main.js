0cf395ff5f614a2916e2a09bb2b2aadf5b67bd4b
ietoto <ietoto@163.com>
2023-03-19 17:00:12 +0800

    登录页面

--- fore-end/src/main.js
+++ fore-end/src/main.js
@@ -1,9 +1,6 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
