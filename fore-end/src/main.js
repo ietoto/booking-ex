e2ff2d1b6b96f613acb49ca24b550cf3a1cfe620
Kevin-Ting <kevin_ting_coding@qq.com>
2023-05-07 19:02:27 +0800

    登陆背景修改

--- fore-end/src/main.js
+++ fore-end/src/main.js
@@ -4,16 +4,14 @@ import router from './router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import store from './store'
-
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
 axios.defaults.baseURL = 'http://localhost:8443/api'
 // 全局注册，之后可在其他组件中通过 this.$axios 发送数据
 Vue.prototype.$axios = axios
 Vue.config.productionTip = false
-
 Vue.use(ElementUI)
-
+// 路由导航守卫
 router.beforeEach((to, from, next) => {
   if (to.meta.requireAuth) {
     if (store.state.user.username) {
--- fore-end/src/router/index.js
+++ fore-end/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
 import Test from '@/components/Test'
@@ -9,11 +8,6 @@ Vue.use(Router)
 export default new Router({
   mode: 'history',
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
     {
       path: '/login',
       name: 'Login',
