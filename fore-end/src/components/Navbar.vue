293dd1679ccee9ef0ea38380f7eebbe8fb452b56
Kevin-Ting <kevin_ting_coding@qq.com>
2023-05-09 00:17:28 +0800

    导航栏

---
+++ fore-end/src/components/Navbar.vue
@@ -0,0 +1,22 @@
+<template>
+    <el-menu>
+
+    </el-menu>
+  </template>
+  
+  <script>
+  export default {
+    name: 'Navbar',
+    data () {
+      // ...
+    },
+    methods: {
+      // ...
+    }
+  }
+  </script>
+  
+  <style scoped>
+  /* ... */
+  </style>
+  
