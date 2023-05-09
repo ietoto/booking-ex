7bf178820f5ad64b974e847a07575594d45cad0d
23retykj <retykj23@163.com>
2023-05-09 13:47:04 +0800

    5.9

--- after-end/src/main/java/com/afterend/controller/SearchController.java
+++ after-end/src/main/java/com/afterend/controller/SearchController.java
@@ -47,12 +47,12 @@ public class SearchController {
         return hotels;
     }
 
-    //只返回客房数量
+    //根据日期和酒店id搜索, 只返回客房数量
     @CrossOrigin
     @PostMapping(value = "/api/search/searchHotelRoomNum")
     @ResponseBody
     public List<Room> searchHotelRoomNum(@RequestBody Search requestSearch, HttpSession session) {
-        System.out.println("Searching...");
+        System.out.println("Searching room number...");
 
         List<Room> rooms = searchService.SearchForHotelRoomNum(requestSearch);
         if(0 == rooms.size()){
@@ -62,5 +62,71 @@ public class SearchController {
         }
         return rooms;
     }
+
+    //根据日期和人数, 搜索酒店id
+    @CrossOrigin
+    @PostMapping(value = "/api/search/searchByDateAndPeople")
+    @ResponseBody
+    public List<Hotel> searchByDateAndPeople(@RequestBody Search requestSearch, HttpSession session) {
+        System.out.println("Searching hotel id...");
+
+        List<Hotel> hotels = searchService.SearchByDateAndHumanNum(requestSearch);
+        if(0 == hotels.size()){
+            System.out.println("No result find.");
+        }else{
+            System.out.println("Results find.");
+        }
+        return hotels;
+    }
+
+    //根据酒店id, 搜索该酒店的设施和客房设施
+    @CrossOrigin
+    @PostMapping(value = "/api/search/searchSingleFac")
+    @ResponseBody
+    public SearchDetailed searchSingleFac(@RequestBody Search requestSearch, HttpSession session) {
+        System.out.println("Searching hotel's facilities...");
+
+        SearchDetailed searchDetailed = searchService.SearchForSingalFac(requestSearch);
+        if(null == searchDetailed){
+            System.out.println("No result find.");
+        }else{
+            System.out.println("Results find.");
+        }
+        return searchDetailed;
+    }
+
+    //根据省份(eg.江苏)日期人数, 搜索酒店
+    @CrossOrigin
+    @PostMapping(value = "/api/search/searchByCityAndDateAndPeople")
+    @ResponseBody
+    public List<Hotel> searchByCityAndDateAndPeople(@RequestBody Search requestSearch, HttpSession session) {
+        System.out.println("Searching hotel List...");
+
+        List<Hotel> hotels = searchService.SearchbyCityAndDateAndHumanNum(requestSearch);
+        if(0 == hotels.size()){
+            System.out.println("No result find.");
+        }else{
+            System.out.println("Results find.");
+        }
+        return hotels;
+    }
+
+    //根据城市(eg.南京)日期人数, 搜索酒店
+    @CrossOrigin
+    @PostMapping(value = "/api/search/searchByLocationAndDateAndPeople")
+    @ResponseBody
+    public List<Hotel> searchByLocationAndDateAndPeople(@RequestBody Search requestSearch, HttpSession session) {
+        System.out.println("Searching hotel List_location...");
+
+        List<Hotel> hotels = searchService.SearchbyLocationAndDateAndHumanNum(requestSearch);
+        if(0 == hotels.size()){
+            System.out.println("No result find.");
+        }else{
+            System.out.println("Results find.");
+        }
+        return hotels;
+    }
+
+
 
 }
