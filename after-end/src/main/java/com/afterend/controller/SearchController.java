07e13e37ff106b07da31145a8c9e29c4261d0a3a
23retykj <retykj23@163.com>
2023-05-09 12:42:23 +0800

    5.9

--- after-end/src/main/java/com/afterend/controller/SearchController.java
+++ after-end/src/main/java/com/afterend/controller/SearchController.java
@@ -39,11 +39,12 @@ public class SearchController {
         System.out.println("Searching...");
 
         List<Hotel> hotels = searchService.SearchbyLocationAndHotel(requestSearch);
-        if(0 == hotels.size()){
-            System.out.println("No result find.");
-        }else{
-            System.out.println("First hotel id: "+hotels.get(0).getId());
-        }
+        System.out.println(hotels.size());
+//        if(0 == hotels.size()){
+//            System.out.println("No result find.");
+//        }else{
+//            System.out.println("First hotel id: "+hotels.get(0).getId());
+//        }
         return hotels;
     }
 
