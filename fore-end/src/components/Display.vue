c363893be8df43684a9bfcdeb174f389d6d7687a
Kevin-Ting <kevin_ting_coding@qq.com>
2023-05-11 10:49:29 +0800

    5.11

--- fore-end/src/components/Display.vue
+++ fore-end/src/components/Display.vue
@@ -1,61 +1,30 @@
 <template>
-    <div>
-        <div>
-            <div>
-                <div class="a826ba81c4 fa71cba65b fa2f36ad22 afd256fc79 d08f526e0d ed11e24d01 ef9845d4b3 bc4e2c467a">
-                    <a target="_isblank"
-                        href="https://www.booking.com/booking-home/index.zh-cn.html?aid=397645&amp;label=bin859jc-1DCAEoggI46AdIK1gDaKoCiAEBmAEruAEYyAEM2AED6AEB-AECiAIBqAIEuAK98eyiBsACAdICJGE2MWYzNTI5LTkyMjAtNGNmYy1hNzQ4LWFkNDZmM2FkYWY5Y9gCBOACAQ"
-                        class="fc63351294 b8e920e088">
-                        <div class="d39c27f89b"></div>
-                        <div class="d875fc0c4c">
-                            <div class="e1f827110f bb44aa69aa">
-                                <div>
-                                    <span>Ting，为下次旅程寻找</span>
-                                    <div class="de7f9979e1">
-                                        <ul class="a2172b8d6f">
-                                            <li class="db46cc1a0a">
-                                                <span class="fc7d18e9e4">公寓</span>
-                                            </li>
-                                            <li class="db46cc1a0a">
-                                                <span class="fc7d18e9e4">别墅</span>
-                                            </li>
-                                            <li class="db46cc1a0a">
-                                                <span class="fc7d18e9e4">公寓式酒店</span>
-                                            </li>
-                                            <li class="db46cc1a0a">
-                                                <span class="fc7d18e9e4">度假屋</span>
-                                            </li>
-                                            <li class="db46cc1a0a">
-                                                <span class="fc7d18e9e4">乡村别墅</span>
-                                            </li>
-                                            <li class="db46cc1a0a">
-                                                <span class="fc7d18e9e4">民宿短租</span>
-                                            </li>
-                                            <li class="db46cc1a0a">
-                                                <span class="fc7d18e9e4">公寓</span>
-                                            </li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <span></span>
-                            </div>
-                            <span class="fc63351294 a822bdf511 e2b4ffd73d f7db01295e c334e6f658 a9a04704ee ccbae5d425">
-                                <span class="e57ffa4eb5">查找民宿短租</span>
-                            </span>
-                        </div>
-                        <div class="a0670806ef">
-                            <picture class="e4e8d3238b">
-                                <img class="c3194e6c89 e5eb7fb6cb"
-                                    src="https://q-xx.bstatic.com/psb/capla/static/media/bh_aw_cpg_main_image.b4347622.png"
-                                    role="presentation" loading="lazy">
-                            </picture>
-                        </div>
-                    </a>
+     <a target="_isblank" href="javascript:;" class="fc63351294 b8e920e088">
+        <div class="d39c27f89b"></div>
+        <div class="d875fc0c4c">
+            <div data-testid="wcu_bh_banner_desktop--content" class="e1f827110f bb44aa69aa">
+                <div><span>为下次旅程寻找</span>
+                    <div class="de7f9979e1">
+                        <ul class="a2172b8d6f">
+                            <li class="db46cc1a0a"><span class="fc7d18e9e4">公寓</span></li>
+                            <li class="db46cc1a0a"><span class="fc7d18e9e4">别墅</span></li>
+                            <li class="db46cc1a0a"><span class="fc7d18e9e4">公寓式酒店</span></li>
+                            <li class="db46cc1a0a"><span class="fc7d18e9e4">度假屋</span></li>
+                        <li class="db46cc1a0a"><span class="fc7d18e9e4">乡村别墅</span></li>
+                        <li class="db46cc1a0a"><span class="fc7d18e9e4">民宿短租</span></li>
+                        <li class="db46cc1a0a"><span class="fc7d18e9e4">公寓</span></li>
+                    </ul>
                 </div>
-            </div>
-        </div>
+            </div><span></span>
+        </div><span data-testid="wcu_bh_banner_desktop--button"
+            class="fc63351294 a822bdf511 e2b4ffd73d f7db01295e c334e6f658 a9a04704ee ccbae5d425"><span
+                class="e57ffa4eb5">查找民宿短租</span></span>
     </div>
-</template>
+    <div class="a0670806ef">
+        <picture class="e4e8d3238b"><img class="c3194e6c89 e5eb7fb6cb"
+                src="../assets/picture/bh_aw_cpg_main_image.b4347622.png" role="presentation" loading="lazy"></picture>
+    </div>
+</a></template>
 
 <script>
 export default {
@@ -63,4 +32,19 @@ export default {
 }
 </script>
 
-<style scoped></style>
+<style scoped>
+.fc63351294 {
+  background: none;
+  border: none;
+  box-sizing: border-box;
+  color: inherit;
+  display: inline-flex;
+  font-size: inherit;
+  font-weight: inherit;
+  padding: 0;
+  position: relative;
+  text-align: start;
+  text-decoration: none;
+  vertical-align: top;
+}
+</style>
--- fore-end/src/components/home/Appindex.vue
+++ fore-end/src/components/home/Appindex.vue
@@ -1,13 +1,33 @@
-<!-- index是登陆后界面 -->
+<!-- index是登陆前界面 -->
 <template>
   <div>
-    Hello World!
+    <Navbar/>
+    <Loginshow/>
+    <Search/>
+    <HotSpot>
+    </HotSpot>
+    <SpotCarousel/>
+    <Display/>
   </div>
 </template>
 
 <script>
+import Display from '../Display.vue'
+import SpotCarousel from '../SpotCarousel.vue'
+import Loginshow from '../Loginshow.vue'
+import Search from '../Search.vue'
+import Navbar from '../Navbar.vue'
+import HotSpot from '../HotSpot.vue'
 export default {
-  name: 'AppIndex'
+  name: 'AppIndex',
+  components: {
+    Navbar,
+    Loginshow,
+    Search,
+    HotSpot,
+    SpotCarousel,
+    Display
+  }
 }
 </script>
 
