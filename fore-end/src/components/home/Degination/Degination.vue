9e197d446392dfd4824e53a742aa77c500fe71f5
Kevin-Ting <kevin_ting_coding@qq.com>
2023-05-12 10:00:18 +0800

    Deginaiton栏的加入

--- fore-end/src/components/home/Degination/Degination.vue
+++ fore-end/src/components/home/Degination/Degination.vue
@@ -1,64 +1,71 @@
 <template>
-    <div style="margin: 0 auto; width: 65%; text-align: left">
-      <h2>Destinations we love</h2>
-      <div class="button-group" style="margin-left: 1.3px">
-        <button 
-          v-for="type in types" 
-          :key="type"
-          class="tab"
-          :class="{active: active === type}"
-          @click="setActive(type)"
-        >
-          {{ type }}
-        </button>
-      </div>
-      <hr style="border: 0.1px solid #0071C2; margin-top: 4px" />
-      <DeginationData :v="active"></DeginationData>
+  <div style="margin: 0 auto; width: 65%; text-align: left">
+    <h2>大家都爱去</h2>
+    <div class="button-group" style="margin-left: 1.3px">
+      <button 
+        v-for="type in types" 
+        :key="type"
+        class="tab"
+        :class="{active: active === type}"
+        @click="setActive(type)"
+      >
+        {{ getButtonTypeText(type) }}
+      </button>
     </div>
-  </template>
-  
-  <script>
-  import DeginationData from './DeginationData'
-  
-  export default {
-    components: {
-      DeginationData
-    },
-    data() {
-      return {
-        active: 'Regions',
-        types: ['Regions', 'Cities', 'Place of interests']
-      }
-    },
-    methods: {
-      setActive(type) {
-        this.active = type
+    <hr style="border: 0.1px solid #0071C2; margin-top: 4px" />
+    <DeginationData :v="active"></DeginationData>
+  </div>
+</template>
+
+<script>
+import DeginationData from './DeginationData'
+
+export default {
+  components: {
+    DeginationData
+  },
+  data() {
+    return {
+      active: 'Regions',
+      types: ['Regions', 'Cities', 'Place of interests'],
+      buttonTexts: {
+        Regions: '地区',
+        Cities: '城市',
+        'Place of interests': '景点'
       }
     }
+  },
+  methods: {
+    setActive(type) {
+      this.active = type
+    },
+    getButtonTypeText(type) {
+      return this.buttonTexts[type]
+    }
   }
-  </script>
-  
-  <style scoped>
-  .tab {
-    margin-right: 15px;
-    font-size: 14px;
-    background: transparent;
-    border: none;
-    outline: none;
-    padding: 5px 10px;
-    cursor: pointer;
-    color: gray;
-    transition: all ease-in-out .2s;
-    border-bottom: 2px solid transparent;
-  }
-  
-  .tab.active {
-    background-color: #0071C2;
-    color: white;
-  }
-  
-  .button-group {
-    display: flex;
-  }
-  </style>
-  
+}
+</script>
+
+<style scoped>
+.tab {
+  margin-right: 15px;
+  font-size: 14px;
+  background: transparent;
+  border: none;
+  outline: none;
+  padding: 5px 10px;
+  cursor: pointer;
+  color: gray;
+  transition: all ease-in-out .2s;
+  border-bottom: 2px solid transparent;
+}
+
+.tab.active {
+  background-color: #0071C2;
+  color: white;
+}
+
+.button-group {
+  display: flex;
+}
+</style>
--- fore-end/src/components/home/Degination/DeginationData.vue
+++ fore-end/src/components/home/Degination/DeginationData.vue
@@ -1,52 +1,50 @@
 <template>
-    <div>
+  <div>
+    <div 
+      v-if="data" 
+      style="margin-top: -10px; display: grid; grid-gap: 20px; grid-template-columns: auto auto auto auto"
+    >
       <div 
-        v-if="data" 
-        style="margin-top: -10px; display: grid; grid-gap: 20px; grid-template-columns: auto auto auto auto"
+        v-for="el in data" 
+        :key="el.name" 
+        style="margin-bottom: -33px; padding: 0"
       >
-        <div 
-          v-for="el in data" 
-          :key="el.name" 
-          style="margin-bottom: -33px; padding: 0"
-        >
-          <p style="color: #4A9AD4; margin-bottom: -12px; fontSize: 15px">{{ el.name }}</p>
-          <p style="color: #A5A5A5; fontSize: 14px">{{ el.properties }}</p>
-        </div>
+        <p style="color: #4A9AD4; margin-bottom: -12px; fontSize: 15px">{{ el.name }}</p>
+        <p style="color: #A5A5A5; fontSize: 14px">{{ el.properties }}</p>
       </div>
     </div>
-  </template>
-  
-  <script>
-  import axios from 'axios'
-  
-  export default {
-    props: ['v'],
-    data() {
-      return {
-        data: []
-      }
-    },
-    watch: {
-      v() {
-        this.getData()
-      }
-    },
-    created() {
+  </div>
+</template>
+
+<script>
+import dbData from "@/assets/Degination.json";
+export default {
+  props: ['v'],
+  data() {
+    return {
+      data: []
+    }
+  },
+  watch: {
+    v() {
       this.getData()
-    },
-    methods: {
-      getData() {
-        axios.get("https://manishsinghbhadouria.github.io/api/db.json").then((response) => {
-          if (this.v === '地区') {
-            this.data = response.data[0].regions
-          } else if (this.v === '城市') {
-            this.data = response.data[0].cities
-          } else {
-            this.data = response.data[0].interests
-          }
-        })
+    }
+  },
+  created() {
+    this.getData()
+  },
+  methods: {
+    getData() {
+      const data = dbData;
+
+      if (this.v === 'Regions') {
+        this.data = data[0].regions
+      } else if (this.v === 'Cities') {
+        this.data = data[0].cities
+      } else {
+        this.data = data[0].interests
       }
     }
   }
-  </script>
-  
+}
+</script>
