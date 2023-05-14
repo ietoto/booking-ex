46a6394690937da3f43a44df1d1a2d42478a1084
ietoto <ietoto@163.com>
2023-05-14 10:42:15 +0800

    登录

--- fore-end/src/components/manage/Hotel_manage.vue
+++ fore-end/src/components/manage/Hotel_manage.vue
@@ -10,90 +10,78 @@
         <el-form  :inline="true">
           <el-row>
             <el-col :span="60" :offset="100">
-              <el-form-item label="用户名：">
-                <el-input v-model="username" placeholder="请输入用户名" clearable/>
+              <el-form-item label="酒店id：">
+                <el-input v-model="hotel_id" placeholder="请输入酒店id" clearable/>
               </el-form-item>
             </el-col>
             <el-col :span="50">
               <el-form-item>
-                <el-button type="primary" icon="el-icon-search" @click="getByUserName">查询</el-button>
+                <el-button type="primary" icon="el-icon-search" @click="getByHotelId">查询</el-button>
                 <el-button type="primary" icon="el-icon-plus" @click="SetAddInformationVisible">新增</el-button>
               </el-form-item>
             </el-col>
           </el-row>
         </el-form>
       </el-header>
-      <el-dialog title="添加个人信息" :visible.sync="AddInformationVisible">
+      <el-dialog title="添加酒店信息" :visible.sync="AddInformationVisible">
         <el-form label-width="100px" style="text-align: center;">
-          <el-form-item label="用户名" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.username"></el-input>
+          <el-form-item label="酒店名" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.name"></el-input>
           </el-form-item>
-          <el-form-item label="密码" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.password"></el-input>
+          <el-form-item label="酒店描述" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.desciption"></el-input>
           </el-form-item>
-          <el-form-item label="姓名" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.name"></el-input>
+          <el-form-item label="距中心距离" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.distance"></el-input>
           </el-form-item>
-          <el-form-item label="电话" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.phone"></el-input>
+          <el-form-item label="所在城市" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.city"></el-input>
           </el-form-item>
-          <el-form-item label="邮箱" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.postbox"></el-input>
+          <el-form-item label="所在地区" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.location"></el-input>
           </el-form-item>
-          <el-form-item label="身份" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-select v-model="adduser.state" placeholder="请选择">
-              <el-option
-                v-for="item in state"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
-            </el-select>
+          <el-form-item label="详细地址" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.address"></el-input>
           </el-form-item>
-          <p v-if="adduser.state===2">
-            <el-form-item label="负责酒店" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-              <el-input v-model="adduser.hotel_id"></el-input>
-            </el-form-item>
-          </p>
+          <el-form-item label="评分" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.score"></el-input>
+          </el-form-item>
+          <el-form-item label="星级" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.star"></el-input>
+          </el-form-item>
           <el-form-item style="width: 60%;display: inline-block;position: relative;left: -50px;">
-            <el-button type="primary" @click="Adduser" style="width: 80%;">提交</el-button>
+            <el-button type="primary" @click="Addhotel" style="width: 80%;">提交</el-button>
           </el-form-item>
         </el-form>
       </el-dialog>
-      <el-dialog title="修改个人信息" :visible.sync="SetInformationVisible">
+      <el-dialog title="酒店详细信息" :visible.sync="ShowInformationVisible">
         <el-form label-width="100px" style="text-align: center;">
-          <el-form-item label="用户名" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.username"></el-input>
+          <el-form-item label="酒店名" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.name" :disabled=ShowInformationIsDisabled></el-input>
           </el-form-item>
-          <el-form-item label="密码" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.password"></el-input>
+          <el-form-item label="酒店描述" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.desciption" :disabled=ShowInformationIsDisabled></el-input>
           </el-form-item>
-          <el-form-item label="姓名" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.name"></el-input>
+          <el-form-item label="距中心距离" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.distance" :disabled=ShowInformationIsDisabled></el-input>
           </el-form-item>
-          <el-form-item label="电话" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.phone"></el-input>
+          <el-form-item label="所在城市" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.city" :disabled=ShowInformationIsDisabled></el-input>
           </el-form-item>
-          <el-form-item label="邮箱" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-input v-model="adduser.postbox"></el-input>
+          <el-form-item label="所在地区" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.location" :disabled=ShowInformationIsDisabled></el-input>
           </el-form-item>
-          <el-form-item label="身份" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-            <el-select v-model="adduser.state" placeholder="请选择">
-              <el-option
-                v-for="item in state"
-                :key="item.value"
-                :label="item.label"
-                :value="item.value">
-              </el-option>
-            </el-select>
+          <el-form-item label="详细地址" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.address" :disabled=ShowInformationIsDisabled></el-input>
           </el-form-item>
-          <p v-if="adduser.state===2">
-            <el-form-item label="负责酒店" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-              <el-input v-model="adduser.hotel_id"></el-input>
-            </el-form-item>
-          </p>
+          <el-form-item label="评分" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.score" :disabled=ShowInformationIsDisabled></el-input>
+          </el-form-item>
+          <el-form-item label="星级" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+            <el-input v-model="addhotel.star" :disabled=ShowInformationIsDisabled></el-input>
+          </el-form-item>
           <el-form-item style="width: 60%;display: inline-block;position: relative;left: -50px;">
-            <el-button type="primary" @click="updateuser" style="width: 80%;">提交</el-button>
+            <el-button type="primary" @click="updatehotel" style="width: 80%;">提交</el-button>
           </el-form-item>
         </el-form>
       </el-dialog>
@@ -114,20 +102,19 @@
               </template>
             </el-table-column>
             <el-table-column label="id" prop="id" min-width="25px"/>
-            <el-table-column label="用户名" prop="username" min-width="25px"/>
-            <el-table-column label="密码" prop="password" min-width="25px"/>
-            <el-table-column label="真实姓名" prop="name" min-width="25px"/>
-            <el-table-column label="邮箱" prop="postbox" min-width="50px"/>
-            <el-table-column label="手机号" prop="phone" min-width="50px"/>
-            <el-table-column label="身份" prop="state" min-width="50px"/>
-            <el-table-column label="管理酒店号" prop="hotel_id" min-width="50px"/>
+            <el-table-column label="酒店名" prop="name" min-width="25px"/>
+            <el-table-column label="所在城市" prop="city" min-width="25px"/>
+            <el-table-column label="所在地区" prop="location" min-width="25px"/>
+            <el-table-column label="距中心距离" prop="distance" min-width="50px"/>
+            <el-table-column label="酒店评分" prop="score" min-width="50px"/>
+            <el-table-column label="酒店星级" prop="star" min-width="50px"/>
             <el-table-column label="操作" prop="operation" width="200">
               <template>
                 <el-button
                   type="text"
                   icon="el-icon-edit"
-                  @click="SetSetInformationVisible"
-                >修改</el-button>
+                  @click="SetShowInformationVisible"
+                >详情</el-button>
                 <el-button
                   type="text"
                   icon="el-icon-delete"
@@ -143,8 +130,8 @@
             @current-change="handleCurrentChange"
             background
             layout="total,prev, pager, next, jumper"
-            :page-size="6"
-            :total="userList.length">
+            :page-size=page.pageSize
+            :total="hotelList.length">
           </el-pagination>
         </div>
       </el-main>
@@ -171,30 +158,17 @@ export default {
       console.log(`当前页: ${val}`);
       this.page.currentPage = val
     },
-    getByUserName(){
+    getByHotelId(){
       var _this = this
       this.loading=true
       this.$axios
-        .post('/user/showbyusername', {
-          username: this.username
+        .post('/hotel/searchById', {
+          id: this.id
         })
         .then(successResponse => {
-          if (successResponse.data !=null) {
+          if (successResponse.data.id !=null) {
             console.log('查询成功')
-            this.userList=successResponse.data
-            for (let i = 0; i < this.userList.length; i++) {
-              switch (this.userList[i].state){
-                case 0:
-                  this.userList[i].state='用户'
-                  break
-                case 1:
-                  this.userList[i].state='系统管理员'
-                  break
-                case 2:
-                  this.userList[i].state='酒店管理员'
-                  break
-              }
-            }
+            this.hotelList.push(successResponse.data)
             this.loading=false
           }
           else {
@@ -206,30 +180,32 @@ export default {
     SetAddInformationVisible(){
       this.AddInformationVisible=true
     },
-    Adduser(){
+    Addhotel(){
       var _this = this
-      if (this.adduser.username === ''||this.adduser.password === ''){
+      if (this.addhotel.name === ''){
         // alert(this.registerForm.phone)
         this.$message({
           duration: 1000,
           showClose: true,
-          message: '用户名或密码不能为空！',
+          message: '酒店名不能为空！',
           type: 'error'
         })
         return
       }
       this.$axios
-        .post('/user/register', {
-          username: this.adduser.username,
-          password: this.adduser.password,
-          name: this.adduser.name,
-          postbox: this.adduser.postbox,
-          phone: this.adduser.phone,
-          state: this.adduser.state,
-          hotel_id: this.adduser.hotel_id
+        .post('/hotel/add', {
+          name: this.addhotel.name,
+          desciption: this.addhotel.desciption,
+          distance: this.addhotel.distance,
+          city: this.addhotel.city,
+          location: this.addhotel.location,
+          address: this.addhotel.address,
+          score: this.addhotel.score,
+          star: this.addhotel.star
+
         })
         .then(successResponse => {
-          if (successResponse.data.username !=null) {
+          if (successResponse.data.code===200) {
             // var data = this.registerForm
             this.$message({
               duration: 1000,
@@ -244,7 +220,7 @@ export default {
             this.$message({
               duration: 1000,
               showClose: true,
-              message: '用户名和密码组合已存在！',
+              message: '添加失败！',
               type: 'error'
             })
           }
