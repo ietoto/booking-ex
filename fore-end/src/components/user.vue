69c5515489e1baa591348c2c720bbf8197e97eee
ietoto <ietoto@163.com>
2023-05-15 20:30:09 +0800

    登录

--- fore-end/src/components/user.vue
+++ fore-end/src/components/user.vue
@@ -1,105 +1,114 @@
 <template>
   <body id="poster">
-    <Navbar />
-    <el-container style="height: 718px; width: 1500px;border: 1px solid #de1b4f;">
-      <el-aside style="width: 300px;margin-top: 50px;border: 1px solid #de1b4f;">
-        <el-container style="height: 60px;width: 200px;border: 1px solid #28de1b;">
-          <el-button type="primary" style="height: 100%;width: 100%;background: #a5c8fa;border: none"
-            v-on:click="setpassword">修改密码</el-button>
-        </el-container>
-        <el-dialog title="密码修改" :visible.sync="SetPasswordVisible">
-          <el-form label-width="100px" style="text-align: center;">
-            <el-form-item label="原始密码" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-              <a v-if="user"><el-input v-model="user.password" :disabled=true></el-input></a>
-            </el-form-item>
-            <el-form-item label="新密码" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-              <a v-if="newpassword"><el-input v-model="newpassword"></el-input></a>
-            </el-form-item>
-            <el-form-item style="width: 60%;display: inline-block;position: relative;left: -50px;">
-              <el-button type="primary" @click="updatePassword()" style="width: 80%;">提交</el-button>
-            </el-form-item>
-          </el-form>
-        </el-dialog>
-        <el-container style="height: 60px;width: 200px;border: 1px solid #28de1b;position: relative;top: 20px;">
-          <el-button type="primary" style="height: 100%;width: 100%;background: #a5c8fa;border: none"
-                     v-on:click="SetInformation">修改信息</el-button>
-        </el-container>
-        <el-dialog title="信息" :visible.sync="SetInformationVisible">
-          <el-form label-width="100px" style="text-align: center;">
-            <el-form-item label="姓名" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-              <a v-if="newname"><el-input v-model="newname"></el-input></a>
-            </el-form-item>
-            <el-form-item label="电话" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-              <a v-if="newphone"><el-input v-model="newphone"></el-input></a>
-            </el-form-item>
-            <el-form-item label="邮箱" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
-              <a v-if="newpostbox"><el-input v-model="newpostbox"></el-input></a>
-            </el-form-item>
-            <el-form-item style="width: 60%;display: inline-block;position: relative;left: -50px;">
-              <el-button type="primary" @click="updateInformation()" style="width: 80%;">提交</el-button>
-            </el-form-item>
-          </el-form>
-        </el-dialog>
+    <Navbar/>
+    <v-sidebar/>
+    <el-container style="height: 625px; width: 1500px;">
+      <el-aside style="width: 250px;margin-top: 50px;">
       </el-aside>
-      <el-container style="height: 718px;border: 1px solid #de1b4f;margin-top: 50px;">
-        <el-descriptions class="margin-top" title="个人信息" :column="3" :data="user">
-          <el-descriptions-item>
-            <template slot="label">
-              <i class="el-icon-user"></i>
-              用户名
-            </template>
-            <a v-if="user">{{ user.username }}</a>
-          </el-descriptions-item>
-          <el-descriptions-item>
-            <template slot="label">
-              <i class="el-icon-mobile-phone"></i>
-              手机号
-            </template>
-            <a v-if="user">{{ user.phone }}</a>
-          </el-descriptions-item>
-          <el-descriptions-item>
-            <template slot="label">
-              <i class="el-icon-location-outline"></i>
-              姓名
-            </template>
-            <a v-if="user">{{ user.name }}</a>
-          </el-descriptions-item>
-          <el-descriptions-item>
-            <template slot="label">
-              <i class="el-icon-tickets"></i>
-              密码
-            </template>
-            <a v-if="user">{{ user.password }}</a>
-          </el-descriptions-item>
-          <el-descriptions-item>
-            <template slot="label">
-              <i class="el-icon-office-building"></i>
-              邮箱
-            </template>
-            <a v-if="user">{{ user.postbox }}</a>
-          </el-descriptions-item>
-          <el-descriptions-item>
-            <template slot="label">
-              <i class="el-icon-office-building"></i>
-              身份
-            </template>
-            <a v-if="user">
-              <a v-if="user.state === 0">用户</a>
-              <a v-else-if="user.state === 1">系统管理员</a>
-              <a v-else-if="user.state === 2">酒店管理员</a>
-            </a>
-          </el-descriptions-item>
-          <a v-if="user"><a v-if="user.state === 2">
+      <el-container style="height: 590px;margin-top: 20px;border: 1px solid rgba(222,27,79,0);width: 1200px;position: relative;left: 20px;">
+        <el-header>
+          <el-form  :inline="true">
+            <el-row>
+              <el-col :span="50">
+                <el-form-item>
+                  <el-button type="primary" icon="el-icon-edit" @click="setpassword">修改密码</el-button>
+                  <el-button type="primary" icon="el-icon-plus" @click="SetInformation">修改信息</el-button>
+                </el-form-item>
+              </el-col>
+            </el-row>
+          </el-form>
+        </el-header>
+        <el-main>
+          <el-dialog title="密码修改" :visible.sync="SetPasswordVisible">
+            <el-form label-width="100px" style="text-align: center;">
+              <el-form-item label="原始密码" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+                <a v-if="user"><el-input v-model="user.password" :disabled=true></el-input></a>
+              </el-form-item>
+              <el-form-item label="新密码" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+                <a v-if="newpassword"><el-input v-model="newpassword"></el-input></a>
+              </el-form-item>
+              <el-form-item style="width: 60%;display: inline-block;position: relative;left: -50px;">
+                <el-button type="primary" @click="updatePassword()" style="width: 80%;">提交</el-button>
+              </el-form-item>
+            </el-form>
+          </el-dialog>
+          <el-dialog title="信息" :visible.sync="SetInformationVisible">
+            <el-form label-width="100px" style="text-align: center;">
+              <el-form-item label="姓名" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+                <a v-if="newname"><el-input v-model="newname"></el-input></a>
+              </el-form-item>
+              <el-form-item label="电话" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+                <a v-if="newphone"><el-input v-model="newphone"></el-input></a>
+              </el-form-item>
+              <el-form-item label="邮箱" prop="pass" style="width: 80%;display: inline-block;position: relative;left: -50px;">
+                <a v-if="newpostbox"><el-input v-model="newpostbox"></el-input></a>
+              </el-form-item>
+              <el-form-item style="width: 60%;display: inline-block;position: relative;left: -50px;">
+                <el-button type="primary" @click="updateInformation()" style="width: 80%;">提交</el-button>
+              </el-form-item>
+            </el-form>
+          </el-dialog>
+          <el-container style="height: 460px;margin-top: -15px;border: 1px solid rgba(222,27,79,0);width: 1200px;position: relative;left: 0px;">
+            <el-descriptions class="margin-top" title="个人信息" :column="3" :data="user">
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-user"></i>
+                  用户名
+                </template>
+                <a v-if="user">{{ user.username }}</a>
+              </el-descriptions-item>
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-mobile-phone"></i>
+                  手机号
+                </template>
+                <a v-if="user">{{ user.phone }}</a>
+              </el-descriptions-item>
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-location-outline"></i>
+                  姓名
+                </template>
+                <a v-if="user">{{ user.name }}</a>
+              </el-descriptions-item>
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-tickets"></i>
+                  密码
+                </template>
+                <a v-if="user">{{ user.password }}</a>
+              </el-descriptions-item>
               <el-descriptions-item>
                 <template slot="label">
                   <i class="el-icon-office-building"></i>
-                  管理的酒店
+                  邮箱
                 </template>
-                <a v-if="user">{{ user.hotel_id }}</a>
+                <a v-if="user">{{ user.postbox }}</a>
               </el-descriptions-item>
-            </a>
-          </a>
-        </el-descriptions>
+              <el-descriptions-item>
+                <template slot="label">
+                  <i class="el-icon-office-building"></i>
+                  身份
+                </template>
+                <a v-if="user">
+                  <a v-if="user.state === 0">用户</a>
+                  <a v-else-if="user.state === 1">系统管理员</a>
+                  <a v-else-if="user.state === 2">酒店管理员</a>
+                </a>
+              </el-descriptions-item>
+              <a v-if="user"><a v-if="user.state === 2">
+                <el-descriptions-item>
+                  <template slot="label">
+                    <i class="el-icon-office-building"></i>
+                    管理的酒店
+                  </template>
+                  <a v-if="user">{{ user.hotel_id }}</a>
+                </el-descriptions-item>
+              </a>
+              </a>
+            </el-descriptions>
+          </el-container>
+        </el-main>
       </el-container>
     </el-container>
   </body>
@@ -107,10 +116,12 @@
 
 <script>
 import Navbar from './home/Navbar/Navbar.vue'
+import vSidebar from './Sidebar.vue';
 export default {
   name: "user",
   components: {
-    Navbar
+    Navbar,
+    vSidebar
   },
   mounted: function () {
     console.log('挂载开始')
