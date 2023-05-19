fd55c52250f0e52bef25493b601cd8db5175d23e
23retykj <retykj23@163.com>
2023-05-19 22:00:20 +0800

    5.19 23reykj url

--- after-end/src/main/java/com/afterend/controller/ImageController.java
+++ after-end/src/main/java/com/afterend/controller/ImageController.java
@@ -25,13 +25,37 @@ public class ImageController {
             System.out.println(id);
             System.out.println(filename);
             String path="";
-            if(id!=0)
-            {
-                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\image\\"+ filename;
-            }else
-            {
-                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\"+ filename;
+            if (id == 0) {
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\0\\"+ filename;
+            }else if(id == 1){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\image\\1000\\"+ filename;
+            }else if (id == 2){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\2000\\"+ filename;
+            }else if (id == 3){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\3000\\"+ filename;
+            }else if (id == 4){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\4000\\"+ filename;
+            }else if (id == 5){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\5000\\"+ filename;
+            }else if(id == 6){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\6000\\"+ filename;
+            }else if (id == 7){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\7000\\"+ filename;
+            }else if (id == 8){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\8000\\"+ filename;
+            }else if (id == 9){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\9000\\"+ filename;
+            }else if (id == 10){
+                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\10000\\"+ filename;
             }
+
+//            if(id!=0)
+//            {
+//                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\image\\"+ filename;
+//            }else
+//            {
+//                path="D:\\Study\\课程资料\\大三下\\软件工程\\实验\\images\\"+ filename;
+//            }
             File file = new File(path);
             InputStream in = new FileInputStream(file);
             byte[] bytes = IOUtils.toByteArray(in);
